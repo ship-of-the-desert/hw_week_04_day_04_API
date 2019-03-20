45eefcdb1014bd5970f5893446c2a3b2a0ded35d
amani11 <amani.g.66@gmail.com>
2019-03-20 22:04:29 +0300

    create a function to display char info based on button click

--- main.js
+++ main.js
@@ -3,17 +3,20 @@
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
 
-function display
+function displayInof(){
+// select container from html
 let container=document.querySelector(".container");
 
 axios({
 method:'GET',
-url:'https://rickandmortyapi.com/api/character/[1,2,3,4,5]'
+url:'https://rickandmortyapi.com/api/character/[1,2,3,4,5]'//use basic url and get first 5 char
 })
 .then(response=>{
-    for (let i=0;i<5;i++){
-        container.innerHTML+=`<p>${response.data[i].name}</p>`
-        container.innerHTML+=`<img src="${response.data[i].image}">`
+    for (let i=0;i<5;i++){//to display info of 5 char
+        container.innerHTML+=`<p>${response.data[i].name}</p>`//display char name in p 
+        container.innerHTML+=`<img src="${response.data[i].image}">`//display char name in img
     } 
 })
-
+}
+//call fun after click on the button from html
+document.querySelector("#random").onclick=displayInof;
