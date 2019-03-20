aae33bb338758a5f727e038c931056e5d980e85a
lamyahamid <lamya.altuwairqi@gmail.com>
2019-03-20 17:02:17 +0300

    amazing homework

--- main.js
+++ main.js
@@ -2,3 +2,26 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+
+
+
+var container=document.querySelector('.container')
+
+
+
+axios({
+    method: 'GET',
+    url: 'https://rickandmortyapi.com/api/character/1,2,3,4,5'
+  })
+
+  .then(response => {
+    for (var i=0; i<5; i++){
+        console.log(response.data[i].name);
+        container.innerHTML+=`<p>${response.data[i].name}</p>`
+        container.innerHTML+=`<img src="${response.data[i].image}"/>`
+      }
+    
+    
+// let char1=document.querySelector('#char1')
+// char1.setAttribute(response.data[0].name)
+  })
