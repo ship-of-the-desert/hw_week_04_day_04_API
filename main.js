a1a2faa8b678398ebc115d4df58e51fb86ad1bce
amani11 <amani.g.66@gmail.com>
2019-03-20 21:49:28 +0300

    used API to display char name and image

--- main.js
+++ main.js
@@ -2,3 +2,18 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+
+function display
+let container=document.querySelector(".container");
+
+axios({
+method:'GET',
+url:'https://rickandmortyapi.com/api/character/[1,2,3,4,5]'
+})
+.then(response=>{
+    for (let i=0;i<5;i++){
+        container.innerHTML+=`<p>${response.data[i].name}</p>`
+        container.innerHTML+=`<img src="${response.data[i].image}">`
+    } 
+})
+
