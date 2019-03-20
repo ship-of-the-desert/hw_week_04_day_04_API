71227538e9eee4af45df2efec49b718c765db990
maramalshalhoub <maram_alshalhoub@hotmail.com>
2019-03-20 22:25:14 +0300

    Few lines left

--- main.js
+++ main.js
@@ -2,3 +2,26 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+
+//create function
+function getImage() {   
+ //must 
+    axios({
+        method: 'get',
+        url: `https://rickandmortyapi.com/api/character/`
+    })
+        .then(response => {
+                for(i=0; i<5; i++)
+                {
+                    var img= document.querySelector('img');
+                    var title= document.querySelector('h5')
+                    img.setAttribute('src', `${response.data.message}`);
+                    title.setAttribute(`${response.data.name}`)
+                }
+        })
+    .catch(error => {
+        console.log(error.message);
+    })
+}
+var button= document.querySelector('#random');
+button.addEventListener('click', getImage);
