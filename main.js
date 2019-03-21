01be626563172a97135a81c0ed83118b05a31c73
alialyami <alidalyami@gmail.com>
2019-03-21 09:54:01 +0300

    Homework Week04 Day04

--- main.js
+++ main.js
@@ -2,3 +2,22 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+
+
+
+
+function rickandmortyapi(){
+axios({
+    method: 'GET',
+    url: `https://rickandmortyapi.com/api/character/1,2,3,4,5`,
+})
+
+    .then(response => {
+        for(var i = 0 ; i < 5 ; i++){
+            document.querySelector(`#image${i}`).setAttribute('src',response.data[i].image)  
+            document.querySelector(`#head${i}`).textContent=response.data[i].name 
+
+        }
+    })
+}
+ 
