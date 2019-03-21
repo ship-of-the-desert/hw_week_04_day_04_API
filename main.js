edca6e6d1a8c509cdb08f15f97d7259a857da945
Meshal505 <rings-3@hotmail.com>
2019-03-21 07:58:56 +0300

    the best i could do

--- main.js
+++ main.js
@@ -2,3 +2,20 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+
+
+
+axios({
+    method: 'GET',
+    url: `https://rickandmortyapi.com/api/character/2`
+
+})
+.then( response => {
+    
+   let $imags = $('.img1');
+   $imags.attr('src', response.data);
+   var rick = await getCharacter(2);
+   })
+
+//
+
